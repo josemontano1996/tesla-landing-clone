6826588d0fb694198414286643b3244627d6a197
Jose Manuel Montano Mengual <josemanuel1996enologia@gmail.com>
2023-11-17 19:29:15 +0100

    added hero section and nav

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,4 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
   content: [
@@ -7,14 +7,21 @@ const config: Config = {
     './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
+    fontFamily: {
+      sans: ['Gotham SSm A', 'sans-serif'],
+    },
     extend: {
+      backgroundColor: {
+        info: 'rgba(34, 34, 34, 0.8)',
+        'info-alt': 'rgba(255, 255, 255, 0.65)',
+      },
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        'gradient-conic': 'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
     },
   },
   plugins: [],
-}
-export default config
+};
+export default config;
+
